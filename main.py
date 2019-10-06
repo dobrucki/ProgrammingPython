75403cae1ed5bf15b069852beac976ededf96e41
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-10-06 22:14:04 +0200

    fullfilled first task's conditions

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ if __name__ == '__main__':
     # First task
     for num, approx in enumerate(wallis(10)):
         print("Przyblizenie nr {0:d} : {1:.5f}".format(num+1, approx))
+
+    # Second task
+    a = 2
+    b = 100
+    print("Liczby pierwsze w przedziale od {0: d} do {1: d}:".format(a, b))
+    print(', '.join(eratostenes(a, b)))
