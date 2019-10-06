9dca5c37d6b700fc850461042086b7fb9ab29d23
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-10-06 22:50:24 +0200

    finished 1, 2

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ from funcs.eratostenes import eratostenes
 if __name__ == '__main__':
     
     # First task
-    for num, approx in enumerate(wallis(10)):
-        print("Przyblizenie nr {0:d} : {1:.5f}".format(num+1, approx))
+    print("\n# Podpunkt 1\n")
+    n = 10
+    print("{0:d} pierwszych przyblizen liczby PI:".format(n))
+    print('\n'.join(map(str, wallis(n))) + '\n')
 
+    a = 84
+    b = 18
+    print("Najwiekszy wspolny dzielnik liczb {} i {} to {}.\n".format(a, b, gcd(a, b)))
+    
     # Second task
+    print("\n# Podpunkt 2\n")
     a = 2
     b = 100
-    print("Liczby pierwsze w przedziale od {0: d} do {1: d}:".format(a, b))
-    print(', '.join(eratostenes(a, b)))
+    print("Liczby pierwsze w przedziale od {0:d} do {1:d}:".format(a, b))
+    print(', '.join(map(str, eratostenes(a, b))))
