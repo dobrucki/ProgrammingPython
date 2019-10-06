15155a3655c463f2652296df1bbbe90f888eac5d
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-10-07 00:31:02 +0200

    finished lcm docs

--- funcs/lcm.py
+++ funcs/lcm.py
@@ -1,6 +1,12 @@
 from funcs.gcd import gcd
 
+
 def lcm(a, b):
+    """Calculates lowest common multiplier of two integers.
+
+    a -- first integer
+    b -- second integer
+    """
     result = a * b
     result /= gcd(a, b)
     return int(result)
